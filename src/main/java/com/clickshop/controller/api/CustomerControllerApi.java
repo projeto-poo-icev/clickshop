c2ec6d2dcc81fad5a13b18c65807e1b392b8d378
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-11-08 17:44:00 -0300

    feat: find all and find by id customer

--- src/main/java/com/clickshop/controller/api/CustomerControllerApi.java
+++ src/main/java/com/clickshop/controller/api/CustomerControllerApi.java
@@ -1,8 +1,11 @@
 package com.clickshop.controller.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +35,15 @@ public class CustomerControllerApi {
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
     }
 
-    
+    @GetMapping(value = "/customer")
+    public ResponseEntity<List<CustomerDto>> findAllCustomers() {
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll());
+    }
+
+    @GetMapping(value = "/customer/{id}")
+    public ResponseEntity<CustomerDto> findCustomerById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findById(id));
+    }
+
+
 }
